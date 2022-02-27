52bfd9b46d6ac252771c12d539e55e6698f99221
Dominik Kaczmarczyk <dominik.kaczmarczyk@smail.th-koeln.de>
2022-02-27 19:52:37 +0100

    -Remove useless classfile

--- app/src/main/java/com/example/stepbook/DataStore.kt
+++
@@ -1,7 +0,0 @@
-package com.example.stepbook
-
-import android.content.Context
-
-class DataStore(val context: Context) {
-    //val Context.datastore:DataStore<Preferences> by preferencesDataStore(name="Stepcount")
-}
